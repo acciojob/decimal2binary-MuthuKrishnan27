a206b63881c25cac6ce074d79dee468ef0308523
MUTHUKRISHNAN S <77878932+MuthuKrishnan27@users.noreply.github.com>
2023-05-25 18:31:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function threeSum(arr, target) {
 // write your code here
-  
+  const n = arr.length;
+
+  // Iterate over all possible combinations of three numbers
+  for (let i = 0; i < n - 2; i++) {
+    for (let j = i + 1; j < n - 1; j++) {
+      for (let k = j + 1; k < n; k++) {
+        if (arr[i] + arr[j] + arr[k] === target) {
+          return 1; // Return 1 (true) if the sum matches the target
+        }
+      }
+    }
+  }
+
+  return 0; // Return 0 (false) if no such combination is found
 }
 
 module.exports = threeSum;
