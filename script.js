024d1d4dbae4b6439978bf6cb8cf01789eece38c
MUTHUKRISHNAN S <77878932+MuthuKrishnan27@users.noreply.github.com>
2023-05-29 19:29:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,4 @@ function decimalToBinary(decimal) {
   return decimal.toString(2);
 }
 
-// Example usage
-console.log(decimalToBinary(10)); // Output: 1010
-console.log(decimalToBinary(42)); // Output: 101010
-
+module.exports=decimalToBinary;
