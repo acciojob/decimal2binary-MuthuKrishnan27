2b6f5a929a53564c375091e818e60b02ba0c47e1
MUTHUKRISHNAN S <77878932+MuthuKrishnan27@users.noreply.github.com>
2023-05-29 19:26:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
-function threeSum(arr, target) {
-// write your code here
-  const n = arr.length;
-
-  // Iterate over all possible combinations of three numbers
-  for (let i = 0; i < n - 2; i++) {
-    for (let j = i + 1; j < n - 1; j++) {
-      for (let k = j + 1; k < n; k++) {
-        if (arr[i] + arr[j] + arr[k] === target) {
-          return 1; // Return 1 (true) if the sum matches the target
-        }
-      }
-    }
-  }
-
-  return 0; // Return 0 (false) if no such combination is found
+function decimalToBinary(decimal) {
+  return decimal.toString(2);
 }
 
-module.exports = threeSum;
+// Example usage
+console.log(decimalToBinary(10)); // Output: 1010
+console.log(decimalToBinary(42)); // Output: 101010
+
